574a1568e7f6b339d09663b5f17eda1755ed3e07
Kened Silva Felix <ksfelix1@stefanini.com>
2022-11-18 21:32:06 -0300

    feito botao novo pedido

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -11,6 +11,7 @@ import {
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
 
 export function Main() {
   return(
@@ -27,7 +28,11 @@ export function Main() {
         
       </Container>
       <Footer>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <Button onPress={() => alert('Novo Pedido')}>
+            Novo Pedido
+            </Button>
+        </FooterContainer>
       </Footer>
     </>
     
